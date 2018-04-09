3cb5da555353cffea07e02106ea4690ce9b4784e
Low Jeng Lam <jenglamlow@gmail.com>
2018-04-09 22:38:31 +0800

    Home Page

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 
 import Table from './Table';
 import Button from './Button';
+import Home from './Home';
 
 const Main = () => (
   <main>
     <Switch>
-      <Route exact path='/' component={Table}/>
+      <Route exact path='/' component={Home}/>
       <Route path='/table' component={Table}/>
       <Route path='/button' component={Button}/>
     </Switch>
