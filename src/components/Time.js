0379e3e517cff902b178bc2fbb6866148960bb12
Low Jeng Lam <jenglamlow@gmail.com>
2018-04-10 23:17:59 +0800

    Populate Timescale

--- src/components/Time.js
+++ src/components/Time.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const canvasHeight = 30;
-const canvasWidthOffset = 182;
+const canvasWidthOffset = 0;
 const canvasStyle = {
   verticalAlign: "bottom"
 };
@@ -67,10 +67,31 @@ class Time extends Component {
   // Helper Functions
   //================================================================================
 
+  msToTimecode(duration, fps) {
+    let frames = parseInt((duration/1000 * fps) % fps, 10);
+    let seconds = parseInt((duration/1000)%60, 10);
+    let minutes = parseInt((duration/(1000*60))%60, 10);
+    let hours = parseInt((duration/(1000*60*60))%24, 10);
+  
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    frames = (frames < 10) ? "0" + frames : frames;
+  
+    // Temporary hardcode the frame to 00
+    return hours + ":" + minutes + ":" + seconds + ":00";
+  }
+
   redrawCanvas() {
     let length = window.innerWidth - canvasWidthOffset;
     this.setState({width: length});
-    this.writeText(length.toString(), 100, 10);
+    // this.writeText(length.toString(), 100, 10);
+    this.drawLine(length);
+    var majorNum = parseInt(length/75, 10) + 1;
+
+    for (let i = 0; i < majorNum; i++) {
+      this.drawScale(i*75, this.msToTimecode(10000 * (i/majorNum), 30), 15, 5);
+    }
   }
 
   writeText(text, x, y) {
@@ -78,6 +99,52 @@ class Time extends Component {
     this.ctx.fillStyle = "#808080";
     this.ctx.textAlign = "center";
     this.ctx.fillText(text ,x, y);
+  }
+
+  drawBar(data) {
+    let barHeight = 7;
+    if (data.type === 'major') {
+      barHeight = 15;
+    }
+    const y0 = canvasHeight - 2;
+    const y1 = y0 - barHeight;
+    const x0 = data.x + 0.5;
+  
+    this.ctx.lineWidth = 1;
+    this.ctx.strokeStyle = "#808080";
+    this.ctx.beginPath();
+    this.ctx.moveTo(x0, y0);
+    this.ctx.lineTo(x0, y1);
+    this.ctx.stroke();
+  
+    if ('text' in data) {
+      this.writeText(data.text, data.x, y1 - 2);
+    }
+  }
+
+  drawLine(length) {
+    const y = canvasHeight;
+    this.ctx.strokeStyle = "#f0f000";
+    this.ctx.lineWidth = 2;
+    this.ctx.beginPath();
+    this.ctx.moveTo(0, y);
+    this.ctx.lineTo(length, y);
+    this.ctx.stroke();
+  }
+
+  drawScale(from, text, step, stepSize) {
+    this.drawBar({
+      type: 'major',
+      x: from,
+      text: text
+    });
+  
+    for (let i = 1; i < step; i++) {
+      this.drawBar({
+        type: 'minor',
+        x: from + stepSize * i
+      });
+    }
   }
 
   render() {
