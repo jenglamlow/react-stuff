93feebf23aae9ec4097f73acc8152f0df743f9de
Low Jeng Lam <jenglamlow@gmail.com>
2018-04-13 17:19:28 +0800

    Styled components example

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const PureComponent = ({className, children}) =>  (
+    <h2 className={className}>{children}</h2>
+);
+
+const StyledComponent = styled(PureComponent)`
+    color: 	DarkSeaGreen 
+`;
 
 const Home = () => (
     <div>
         <h1>React Stuff Home </h1>
+        <StyledComponent>Pure Component</StyledComponent>
     </div>
 );
 
