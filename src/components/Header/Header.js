ba9f4b5aee00912c72bc4d1ac6f63ed6f0c8084c
Low Jeng Lam <jenglamlow@gmail.com>
2018-04-17 15:19:35 +0800

    Change Header Style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 // The Header creates links that can be used to navigate
 // between routes.
-const Header = () => (
-  <header>
-    <nav>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/table'>Table</Link></li>
-        <li><Link to='/button'>Button</Link></li>
-        <li><Link to='/time'>Time</Link></li>
-        <li><Link to='/timeline'>Timeline</Link></li>
-      </ul>
-    </nav>
+
+const HeaderComponent = ({className})  => (
+  <header className={className}>
+    <Link to='/'>Home</Link>
+    <Link to='/table'>Table</Link>
+    <Link to='/button'>Button</Link>
+    <Link to='/time'>Time</Link>
+    <Link to='/timeline'>Timeline</Link>
   </header>
 );
+
+const Header = styled(HeaderComponent)`
+  margin: 15px 
+`;
+
 
 export default Header;
