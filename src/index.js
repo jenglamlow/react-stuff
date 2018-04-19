51f0edd9ffc952c6f3c2b30585214c61fff2810d
Low Jeng Lam <jenglamlow@gmail.com>
2018-04-19 18:21:50 +0800

    fix router link when deploy

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
-    <BrowserRouter>
+    <BrowserRouter basename="/react-stuff">
       <App />
     </BrowserRouter>
   ), document.getElementById('root'));
