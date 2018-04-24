6818625cc1beb0e4952d22cd23bff929a3c68d76
Low Jeng Lam <jenglamlow@gmail.com>
2018-04-24 15:56:54 +0800

    Hover feature

--- src/components/ReactTableExample.js
+++ src/components/ReactTableExample.js
@@ -5,50 +5,66 @@ import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 import './react-table.css';
 
+const data = [{
+  name: 'Tanner Linsleyasdddddddddddddddddddddddddddddadasdddddddddddddsasdddddddddddddddddddddddddd',
+  age: 26,
+  friend: {
+    name: 'Jason Maurer',
+    age: 23,
+  }
+}, {
+  name: 'Tanner Linsley',
+  age: 26,
+  friend: {
+    name: 'Jason Maurer',
+    age: 23,
+  }
+}];
+
+const columns = [{
+  Header: 'Name',
+  accessor: 'name' // String-based value accessors!
+}, {
+  Header: 'Age',
+  accessor: 'age',
+  Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+}, {
+  id: 'friendName', // Required because our accessor is not a string
+  Header: 'Friend Name',
+  accessor: d => d.friend.name // Custom value accessors!
+}, {
+  Header: props => <span>Friend Age</span>, // Custom header components!
+  accessor: 'friend.age'
+}];
 
 class ReactTableExample extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      highlightedIndex: -1
+    };
+  }
+
   render() {
-    const data = [{
-      name: 'Tanner Linsley',
-      age: 26,
-      friend: {
-        name: 'Jason Maurer',
-        age: 23,
-      }
-    }, {
-      name: 'Tanner Linsley',
-      age: 26,
-      friend: {
-        name: 'Jason Maurer',
-        age: 23,
-      }
-    }];
-
-    const columns = [{
-      Header: 'Name',
-      accessor: 'name' // String-based value accessors!
-    }, {
-      Header: 'Age',
-      accessor: 'age',
-      Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
-    }, {
-      id: 'friendName', // Required because our accessor is not a string
-      Header: 'Friend Name',
-      accessor: d => d.friend.name // Custom value accessors!
-    }, {
-      Header: props => <span>Friend Age</span>, // Custom header components!
-      accessor: 'friend.age'
-    }];
-
     return (
       <div>
         <ReactTable
           showPagination={false}
           data={data}
-          columns={columns} 
+          minRows = {0}
+          columns={columns}
           className="-highlight"
-          style={{
-            color: "lightgrey"
+          getTrGroupProps={(params, rowInfo) => {
+            let style = {};
+            if (rowInfo.index === this.state.highlightedIndex) {
+              style = { border: "1px solid lightblue" };
+            }
+            return {
+              style,
+              onClick: () => {
+                this.setState({ highlightedIndex: rowInfo.index });
+              }
+            };
           }}
         />
       </div>
